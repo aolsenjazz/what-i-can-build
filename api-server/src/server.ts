7e433081d2c59d2f7550018082e9361e038322a6
Alexander Olsen <aolsenjazz@gmail.com>
2024-08-12 10:12:49 -0400

    feature(partial): morgan integrated logging

--- api-server/src/server.ts
+++ api-server/src/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 
 import { logger } from './logger';
+import { morganMiddlware } from './middleware/morgan-middleware';
 
 import AppDataSource from './database';
 import { UserRepository } from './repository/user-repository';
@@ -9,10 +11,11 @@ import { UserRepository } from './repository/user-repository';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(morganMiddlware);
 
 app.get('/', async (req, res) => {
   const users = await UserRepository.find();
-
   res.json(users);
 });
 
